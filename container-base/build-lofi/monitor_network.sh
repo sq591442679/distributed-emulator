0caedd603521ff389e7413ab694fcd60545d89c7
sq591442679 <591442679@qq.com>
2024-03-29 17:41:02 +0800

    [dev](monitor_network.sh) add monitor for routing table change

--- container-base/build-lofi/monitor_network.sh
+++ container-base/build-lofi/monitor_network.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 LOG_FILE="/var/log/network_events.log"
+LAST_HASH=""
 
 log_interface() {
     local event=$1
@@ -18,9 +19,12 @@ ip monitor | while read -r line; do
     if [[ "$line" == *"state"* ]]; then
         interface=$(echo "$line" | awk -F '@if' '{print $1}' | awk '{print $NF}')
         state=$(echo "$line" | grep -oP "(?<=state )[^ ]+")
-
         log_interface "Interface state changed to $state" "$interface"
-    elif [[ "$line" == *"zebra"* ]]; then
-        log_routing_table
+    elif [[ "$line" == *"id"* ]]; then
+        CURRENT_HASH=$(ip route | md5sum)
+        if [ "$LAST_HASH" != "$CURRENT_HASH" ]; then
+            log_routing_table
+            LAST_HASH=$CURRENT_HASH
+        fi
     fi
 done
--- node-manager/main.py
+++ node-manager/main.py
@@ -297,11 +297,10 @@ if __name__ == "__main__":
 
     enable_load_awareness = False
     lofi_delta = 0.05
-    # link_failure_rate_list = [0, 0.01, 0.02, 0.03, 0.04, 0.05]
-    lofi_n_list = [0]
-    link_failure_rate_list = [0.05]
+    link_failure_rate_list = [0, 0.01, 0.05]
+    lofi_n_list = [5]
     # lofi_n_list = [-1, 2, 4]
-    test_nums = [5]
+    test_nums = [10]
 
     if (len(lofi_n_list) != len(test_nums)):
         raise Exception('lofi_n_list and test_nums not correspond')
