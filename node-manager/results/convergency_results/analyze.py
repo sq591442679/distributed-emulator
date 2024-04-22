201d9730b3066a281f4d8550024a913c41105b76
sq591442679 <591442679@qq.com>
2024-04-22 20:02:43 +0800

    [result](/convergeency_results):n=1 result

--- node-manager/results/convergency_results/analyze.py
+++ node-manager/results/convergency_results/analyze.py
@@ -2,7 +2,7 @@ import os
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
-with open(f'{current_directory}/n=2.txt', 'r') as f:
+with open(f'{current_directory}/n=1.txt', 'r') as f:
 	total_lsu_size = 0
 	for line in f:
 		if 'lsu size' in line:
